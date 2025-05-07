2f38db1a04f18d9d59599b8762589e15dd5a901c
unknown <gaonenggg@outlook.com>
2025-05-07 09:30:27 +0800

    feat: dialog image

--- src/pages/dialog.vue
+++ src/pages/dialog.vue
@@ -10,11 +10,10 @@ const selectWindowClose = () => {
 
 <template>
   <div class="w-fit px-4 py-2 bg-zinc-200 rounded-lg space-y-2 bg-opacity-25 cursor-pointer" data-tauri-drag-region>
-    <div class="size-64px bg-red-500" data-tauri-drag-region>
-      <!-- 替换为某一个标签 -->
-      <!-- <img src="" alt=""> -->
+    <div class="size-64px flex items-center justify-center" data-tauri-drag-region>
+      <img src="/img.png" alt="">
     </div>
-    <button class="w-full bg-blue-500 text-zinc-200 px-3 py-1 rounded-md border-none" @click="selectWindowClose">
+    <button class="cursor-pointer w-full bg-blue-500 text-zinc-200 px-3 py-1 rounded-md border-none" @click="selectWindowClose">
       确认
     </button>
   </div>
