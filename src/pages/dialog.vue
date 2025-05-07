37c25709baa78f94a9d471e757f1be956ac8a1ea
zrll_ <46812903+zrll12@users.noreply.github.com>
2025-05-07 14:28:45 +0800

    fix: can drag target icon

--- src/pages/dialog.vue
+++ src/pages/dialog.vue
@@ -11,7 +11,7 @@ const selectWindowClose = () => {
 <template>
   <div class="w-fit px-4 py-2 bg-zinc-200 rounded-lg space-y-2 bg-opacity-25 cursor-pointer" data-tauri-drag-region>
     <div class="size-64px flex items-center justify-center" data-tauri-drag-region>
-      <img src="/img.png" alt="">
+      <img src="/img.png" alt="" @dragstart.prevent data-tauri-drag-region>
     </div>
     <button class="cursor-pointer w-full bg-blue-500 text-zinc-200 px-3 py-1 rounded-md border-none" @click="selectWindowClose">
       确认
