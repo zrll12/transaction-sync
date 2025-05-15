1446ff0fd008f5a68cb5539eb0911d9e09ecd948
GaoNeng-wWw <gaonenggg@outlook.com>
2025-05-15 15:52:29 +0800

    fix: ci

--- ci/install-choco.sh
+++ ci/install-choco.sh
@@ -9,3 +9,17 @@ export PATH="/C/tools/opencv/build/x64/vc16/bin:/C/tools/opencv/build/x64/vc15/b
 export OPENCV_LINK_PATHS="C:/tools/opencv/build/x64/vc16/lib,C:/tools/opencv/build/x64/vc15/lib"
 export OPENCV_LINK_LIBS="opencv_world${OPENCV_VERSION//./}"
 export OPENCV_INCLUDE_PATHS="C:/tools/opencv/build/include"
+
+echo "=== Current directory: $(pwd)"
+echo "=== Environment variable dump:"
+export
+echo "=== Target settings:"
+rustc --version
+rustc --print=cfg
+
+export RUST_BACKTRACE=full
+
+export CXX=clang++
+
+pnpm i
+pnpm tauri build
