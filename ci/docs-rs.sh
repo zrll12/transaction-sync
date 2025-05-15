7d281f544764fb93136922e56db8bc4cee2ca21a
GaoNeng-wWw <gaonenggg@outlook.com>
2025-05-15 13:57:07 +0800

    fix: ci

--- ci/docs-rs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -xeu
-
-sudo apt-get update
-sudo apt-get install -y clang libclang-dev
-
-export RUST_BACKTRACE=full DOCS_RS=1
-cargo doc -vv
--- ci/install.sh
+++ ci/install.sh
@@ -4,37 +4,10 @@ set -xeu
 
 ci_dir="$(dirname "$0")"
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	os_family="Linux"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-	os_family="macOS"
-elif [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
-	os_family="Windows"
-elif [[ "$OSTYPE" == "freebsd"* ]]; then
-	exit "FreeBSD is not supported"
-else
-	exit "Unknown OS: $OSTYPE"
-fi
+export CHOCO_LLVM_VERSION=20.1.0
 
-if [[ "$os_family" == "Linux" ]]; then
-	if [[ "${VCPKG_VERSION:-}" != "" ]]; then # vcpkg build
-		"$ci_dir/install-ubuntu-vcpkg.sh"
-	else
-		"$ci_dir/install-ubuntu.sh"
-	fi
-elif [[ "$os_family" == "macOS" ]]; then
-	if [[ "${BREW_OPENCV_VERSION:-}" != "" ]]; then # brew build
-		"$ci_dir/install-macos-brew.sh"
-	elif [[ "${VCPKG_VERSION:-}" != "" ]]; then # vcpkg build
-		"$ci_dir/install-macos-vcpkg.sh"
-	else
-		"$ci_dir/install-macos-framework.sh"
-	fi
-elif [[ "$os_family" == "Windows" ]]; then
-	export CHOCO_LLVM_VERSION=20.1.0 # https://community.chocolatey.org/packages/llvm#versionhistory
-	if [[ "${VCPKG_VERSION:-}" != "" ]]; then # vcpkg build
-		"$ci_dir/install-windows-vcpkg.sh"
-	else # chocolatey build
-		"$ci_dir/install-windows-chocolatey.sh"
-	fi
+if [[ "${VCPKG_VERSION:-}" != "" ]]; then # vcpkg build
+	"$ci_dir/install-windows-vcpkg.sh"
+else # chocolatey build
+	"$ci_dir/install-windows-chocolatey.sh"
 fi
