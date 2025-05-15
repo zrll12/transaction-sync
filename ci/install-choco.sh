1669c01a6efef2b1b7d6aceedd7ce9316be20cf3
GaoNeng-wWw <gaonenggg@outlook.com>
2025-05-15 15:20:49 +0800

    fix:ci

---
+++ ci/install-choco.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -xeu
+
+choco install -y llvm
+choco install -y opencv --version=4.11.0
+
+export PATH="/C/tools/opencv/build/x64/vc16/bin:/C/tools/opencv/build/x64/vc15/bin:$PATH"
+export OPENCV_LINK_PATHS="C:/tools/opencv/build/x64/vc16/lib,C:/tools/opencv/build/x64/vc15/lib"
+export OPENCV_LINK_LIBS="opencv_world${OPENCV_VERSION//./}"
+export OPENCV_INCLUDE_PATHS="C:/tools/opencv/build/include"
